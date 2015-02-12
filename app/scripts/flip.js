bbb8b6a27988f73fff0b98ba00b7e3cb249324d3
Omar Sainz <SQL_Slammer.exe@live.de>
2015-02-12 16:07:23 -0600

    Docs Section modified with sliders

---
+++ app/scripts/flip.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$(document).on('ready', function() {
+	$('#powerDocs').on('shown.bs.modal', function () {
+	});
+	$('#ekcosDocs').on('shown.bs.modal', function () {
+	});
+	$('#ezDocs').on('shown.bs.modal', function () {
+	});
+	$('#truDocs').on('shown.bs.modal', function () {
+	});
+	$('.flipster-power').flipster({ style: 'coverflow'});
+});
